37ab4ebeb998a52b588ab2db2ed762ec7c48a08f
Rohit Bhardwaj <150109619+rohitkrbhardwaj09@users.noreply.github.com>
2024-03-19 09:47:46 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,3 +18,36 @@ let someNumber = 33;
 let stringNum = String(someNumber)
 console.log(stringNum) //33
 console.log(typeof stringNum) //string
+
+//*************************************OPERATIONS***********************************************************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue) //-3
+
+let str1 = "Rohit"
+let str2 = " Bhardwaj"
+let str3 = str1+str2
+console.log(str3); //Rohit Bhardwaj
+
+console.log("1"+2) //12
+console.log(1+"2"); //12
+console.log("1"+"2"); //12
+console.log("1"+2+3); //123
+console.log(1+2+"3"); //33
+
+console.log(+true) //1
+
+console.log(+"") //0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); //101
+
+let gameCounter2 = 200
+++gameCounter2;
+console.log(gameCounter2);
+

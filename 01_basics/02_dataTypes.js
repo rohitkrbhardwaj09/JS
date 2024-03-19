f6cc74be2bdfbf674f207bd0873bebd27d0e9fb8
Rohit Bhardwaj <150109619+rohitkrbhardwaj09@users.noreply.github.com>
2024-03-19 06:35:00 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") // This will throw error because, we are using nodejs, not browser
+//DataTypes
+//String ==> ""
+//number - Range: 2 to the power of 53
+//bigint
+//boolean ==> true and false
+//null ==> standalone value
+//undefined ==> value not defined yet
+//symbol ==> uniqueness
+
+//object
+
+ let name = "Rohit" 
+ let age = 27 
+ let loggedIn = false 
+
+ console.log(typeof null) //object
+ console.log(typeof undefined) //undefined
+ console.log(typeof loggedIn) //boolean
+ console.log(typeof age)
+ console.log(typeof name)

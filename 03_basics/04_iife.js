699bf76ca90951bb490ae8d446278264ef4ba3d0
Rohit Bhardwaj <150109619+rohitkrbhardwaj09@users.noreply.github.com>
2024-03-21 06:45:12 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,9 @@
+//Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    console.log(`DB Connected`);
+})();
+
+((name) => {
+    console.log(`DB Connected two ${name}`);
+})('Rohit')
